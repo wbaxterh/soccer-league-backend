5ec09e2ce4874659b84d0eabfb5b24831b59456d
wbaxterh <wesleybaxterhuber@gmail.com>
2025-06-26 18:18:23 -0400

    update to not create tables on deploy - api reference in server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import app from "./app";
 
-const PORT = process.env.PORT || 3010;
+// Only start the server locally if not running in Lambda
+if (process.env.AWS_LAMBDA_FUNCTION_NAME === undefined) {
+	const PORT = process.env.PORT || 3010;
+	app.listen(PORT, () => {
+		console.log(`Server is running on port ${PORT}`);
+	});
+}
 
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
-});
+// Export Lambda handler for AWS
+import serverlessExpress from "@vendia/serverless-express";
+export const handler = serverlessExpress({ app });

cc7b7718eec7517cab2c2c267efbfa06fefcdcb3
wbaxterh <wesleybaxterhuber@gmail.com>
2025-06-27 09:06:20 -0400

    only use aws creds running locally

--- src/utils/dynamoClient.js
+++ src/utils/dynamoClient.js
@@ -1,15 +1,20 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const { DynamoDBDocumentClient } = require("@aws-sdk/lib-dynamodb");
 
+const isLambda = !!process.env.AWS_LAMBDA_FUNCTION_NAME;
+
 const client = new DynamoDBClient({
 	region: process.env.AWS_REGION,
-	credentials:
-		process.env.AWS_ACCESS_KEY_ID && process.env.AWS_SECRET_ACCESS_KEY
-			? {
+	...(!isLambda &&
+	process.env.AWS_ACCESS_KEY_ID &&
+	process.env.AWS_SECRET_ACCESS_KEY
+		? {
+				credentials: {
 					accessKeyId: process.env.AWS_ACCESS_KEY_ID,
 					secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-			  }
-			: undefined,
+				},
+		  }
+		: {}),
 });
 
 const ddbDocClient = DynamoDBDocumentClient.from(client);
